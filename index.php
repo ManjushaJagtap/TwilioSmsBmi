e937d25f56a3f12d00934b0551a8f2ab9b2b183e
Pinak Honmode <pinak.honmode2602@gmail.com>
2018-10-17 03:22:35 -0400

    tele

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// Get the PHP helper library from https://twilio.com/docs/libraries/php
+
+require_once 'vendor/autoload.php'; // Loads the library
+use Twilio\Twiml;
+
+$response = new Twiml;
+$body = $_REQUEST['Body'];
+if( $body == 'hello' ){
+    $response->message('Hi!');
+}else if( $body == 'bye' ){
+    $response->message('Goodbye');
+}
+print $response;
+
+
+?>
