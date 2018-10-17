ef875b997a8e3fa0f5bee19479a550c30847c822
Manjusha Jagtap <manjusha.jagtap2010@gmail.com>
2018-10-17 14:14:50 -0400

    test

--- index.php
+++ index.php
@@ -14,19 +14,8 @@ $token = "aea5ba68af2f804d7700077f93df54ef";
 $client = new Client($sid, $token);
 
 
-$responseMessages = array(
-    'monkey'    => array('body' => 'Monkey. A small to medium-sized primate that typically has a long tail, most kinds of which live in trees in tropical countries.', 
-                         'media' => 'https://cdn.pixabay.com/photo/2016/02/12/23/49/scented-monkey-1197100_960_720.jpg'),
-    'dog'       => array('body' => 'Dog. A domesticated carnivorous mammal that typically has a long snout, an acute sense of smell, and a barking, howling, or whining voice.',
-                         'media' => 'https://cdn.pixabay.com/photo/2016/10/15/12/01/dog-1742295_960_720.jpg'),
-    'pigeon'   => array('body' => 'Pigeon. A stout seed- or fruit-eating bird with a small head, short legs, and a cooing voice, typically having gray and white plumage.',
-                         'media' => 'https://cdn.pixabay.com/photo/2016/11/17/21/12/pigeon-1832742_960_720.jpg'),
-    'owl'       => array('body' => 'Owl. A nocturnal bird of prey with large forward-facing eyes surrounded by facial disks, a hooked beak, and typically a loud call.',
-                         'media' => 'https://cdn.pixabay.com/photo/2013/02/04/20/48/owl-77894_960_720.jpg')
-);
 
 
-$defaultMessage = "Reply with one of the following keywords: monkey, dog, pigeon, owl.";
 
 $body = $_REQUEST['Body']; 
 $to = $_REQUEST['From'];
@@ -45,39 +34,5 @@ if($height !="1")
         )
     );
 }
-$result = preg_replace("/[^A-Za-z0-9]/u", " ", $body); 
-$result = trim($result); 
-$result = strtolower($result); 
-$sendDefault = true; 
 
-foreach ($responseMessages as $animal => $messages) {
-    if ($animal == $result) {
-        $body = $messages['body'];
-        $media = $messages['media'];
-		$sendDefault = false;
-		
-    }
-}
-
-
-if ($sendDefault != false) {
-	
-    $client->messages->create(
-        $to,
-        array(
-            'from' => $from,
-            'body' => $defaultMessage,
-        )
-    );
-} else {
-	
-    $client->messages->create(
-        $to,
-        array(
-            'from' => $from,
-            'body' => $body,
-            'mediaUrl' => $media,
-        )
-    );
-}
 ?>
